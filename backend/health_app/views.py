2e0927dd5e73023f5b7d4b5187b943d33d7af481
maurinenami <maurinenami>
2019-07-25 12:45:21 +0300

    added navbar

--- backend/health_app/views.py
+++ backend/health_app/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
 
-
+@login_required(login_url='/accounts/login/')
 def welcome(request):
     
 
