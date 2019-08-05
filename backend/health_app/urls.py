0442f657f7498c715cd88a75bf4b4dc917636155
kennymaina <victormurigi@outlook.com>
2019-08-05 10:58:59 +0300

    trial branch

--- backend/health_app/urls.py
+++ backend/health_app/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
+    
     url(r'^api/profile/$',views.ProfileList.as_view()),
     url(r'^api/original/$',views.OriginalList.as_view()),
     url(r'^api/extracts/$',views.ExtractedList.as_view()),
